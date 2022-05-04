4805c689f23d4ae34b934646928f505fb59ed0fd
Lucas Nahuel Lucero <lucaslucero2003@hotmail.com>
2022-05-04 20:48:16 -0300

    Se agrego el Proyecto

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i;
+
+    for(i=1;i<=9;i++)
+    {
+        printf("\n");
+        printf("%d ",i);
+
+        printf("%d ",i*2);
+
+        printf("%d ",i*3);
+
+        printf("%d ",i*4);
+
+        printf("%d ",i*5);
+
+        printf("%d ",i*6);
+
+        printf("%d ",i*7);
+
+        printf("%d ",i*8);
+
+        printf("%d ",i*9);
+
+    }
+    return 0;
+}
